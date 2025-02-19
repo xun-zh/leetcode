79f391907287dd513892bfbcb1264584e8e03b9d
xun <xun.zhang0506@gmail.com>
2025-02-19 21:45:28 +0800

    KMP

--- 28.找出字符串中第一个匹配项的下标.cpp
+++ 28.找出字符串中第一个匹配项的下标.cpp
@@ -1,7 +1,7 @@
 // @before-stub-for-debug-begin
-#include <vector>
-#include <string>
 #include "commoncppproblem28.h"
+#include <string>
+#include <vector>
 
 using namespace std;
 // @before-stub-for-debug-end
@@ -15,9 +15,39 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int strStr(string haystack, string needle) { // TODO: KMP
-        
+  int strStr(string haystack, string needle) {
+    if (needle.size() == 0) {
+      return 0;
     }
+    // next
+    vector<int> next(needle.size(), 0);
+    // HINT: pre_tail: 指向前缀末尾，也是最长相等前后缀的长度
+    // HINT: post_tail: 指向后缀末尾，post及post之前也指当前求的子串
+    for (int post_tail = 1, pre_tail = 0; post_tail < needle.size();
+         post_tail++) {
+      while (pre_tail > 0 && needle[post_tail] != needle[pre_tail]) {
+        // NOTE: 不相等时，说明前后缀不匹配了-> 前缀太长，
+        // NOTE: 回溯到上一个最长相等前后缀的长度，再次比较
+        pre_tail = next[pre_tail - 1];
+      }
+      if (needle[post_tail] == needle[pre_tail]) {
+        pre_tail++;
+      }
+      next[post_tail] = pre_tail;
+    }
+    // match
+    for (int i = 0, j = 0; i < haystack.size(); i++) {
+      while (j > 0 && haystack[i] != needle[j]) {
+        j = next[j - 1];
+      }
+      if (haystack[i] == needle[j]) {
+        j++;
+      }
+      if (j == needle.size()) {
+        return i - needle.size() + 1;
+      }
+    }
+    return -1;
+  }
 };
 // @lc code=end
-
--- 459.重复的子字符串.cpp
+++ 459.重复的子字符串.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include "commoncppproblem459.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode.cn id=459 lang=cpp
  *
@@ -7,8 +15,40 @@
 // @lc code=start
 class Solution {
 public:
-  bool repeatedSubstringPattern(string s) { // TODO KMP
-    return (s + s).find(s, 1) != s.size();
+  bool repeatedSubstringPattern(string s) { // TODO 理解 next 数组的右移
+    // NOTE: 加入 s 由 多个 a 串组成，即 s = aa...a
+    // NOTE: 则 s + s == aa...aaa...a，必然出现 s (去除s的首尾避免找到原本的 s)
+    // HACK: 反过来，如果 s + s 中出现了 s，则 s 一定由重复子串组成吗？
+    // NOTE:
+    auto str = (s + s).substr(1, s.size() * 2 - 2);
+    return kmp(str, s);
+  }
+
+  bool kmp(string str, string &pattern) {
+    // get next
+    vector<int> next(pattern.size(), -1);
+    for (int i = 1, j = -1; i < pattern.size(); i++) {
+      while (j != -1 && pattern[j + 1] != pattern[i]) {
+        j = next[j];
+      }
+      if (pattern[j + 1] == pattern[i]) {
+        j++;
+        next[i] = j;
+      }
+    }
+    // match
+    for (int i = 0, j = -1; i < str.size(); i++) {
+      while (j != -1 && str[i] != pattern[j + 1]) {
+        j = next[j];
+      }
+      if (str[i] == pattern[j + 1]) {
+        j++;
+      }
+      if (j == pattern.size() - 1) {
+        return true;
+      }
+    }
+    return false;
   }
 };
 // @lc code=end
